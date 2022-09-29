499de4db02680378b40812c64a04bbe929400954
andreas <andshmoon@gmail.com>
2022-09-30 00:09:25 +0900

    Serverless-API

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const serverless = require("serverless-http");
 const express = require("express");
 const app = express();
 
+//http://localhost:3000/dev//
 app.get("/", function (req, res) {
   res.send("Hello World!");
 });
 
+//http://localhost:3000/dev/dev
+app.get("/dev", function (req, res) {
+  res.send("Hello World!");
+});
 module.exports.handler = serverless(app);

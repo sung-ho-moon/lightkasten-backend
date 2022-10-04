d1c4d0e12f753dff8486bc6e9801ef14189bc501
andreas <andshmoon@gmail.com>
2022-10-05 00:55:36 +0900

    connected mongoose(not checked), ES Module Code

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
+require("dotenv").config();
 //serverless framework
 const serverless = require("serverless-http");
 
 const koa = require("koa");
 const Router = require("koa-router");
 const bodyParser = require("koa-bodyparser");
+const mongoose = require("mongoose");
+
+const { PORT, MONGO_URI } = process.env;
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch((e) => {
+    console.error(e);
+  });
 
 const app = new koa();
 const router = new Router();
@@ -15,8 +28,10 @@ app.use(bodyParser());
 
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(4000, () => {
-  console.log("Listening to port 4000");
+const port = PORT || 4000;
+app.listen(PORT, () => {
+  //sls offline이라서 쉽지않네
+  console.log(`Listening to port ${port}`);
 });
 
 //serverless hadnler exports
